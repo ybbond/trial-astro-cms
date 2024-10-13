9ff44934c1cbe72ad8f57c360fa77ddb2f9aacaf
Yohanes Bandung Bondowoso <hi@ybbond.id>
2024-10-13 21:09:27 +0700

    feat: added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
+import netlify from '@astrojs/netlify';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
+  integrations: [react()],
+  output: 'server',
+  adapter: netlify()
 });
